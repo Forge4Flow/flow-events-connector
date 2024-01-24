64cd5256877a7a0e2ffec3b496ca283fb6479523
BoiseITGuru <dev@boiseitguru.com>
2024-01-23 17:05:15 -0800

    updated defualt values

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/openfaas/connector-sdk/types"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/rs/zerolog/pkgerrors"
@@ -18,6 +17,10 @@ const (
 	DefaultDatastoreMigrationSource = "github://forge4flow/flow-events-connector/internal/database/migrations"
 	DefaultUserAgent                = "forge4flow/flow-events-connector@v0.0.1"
 	DefaultTopic                    = "flow-events"
+	DefaultMainnetAccessNode        = "access.mainnet.nodes.onflow.org:9000"
+	DefaultTestnetAccessNode        = "access.testnet.nodes.onflow.org:9000"
+	DefaultCrescendoAccessNode      = "access.crescendo.nodes.onflow.org:9000"
+	DefaultEmulatorAccessNode       = ""
 	Prefix                          = "flow_events"
 )
 
@@ -72,6 +75,12 @@ func NewConfig() FlowEventsConnectorConfig {
 	viper.SetDefault("datastore.migrationSource", DefaultDatastoreMigrationSource)
 	viper.SetDefault("controller.userAgent", DefaultUserAgent)
 	viper.SetDefault("controller.topic", DefaultTopic)
+	viper.SetDefault("flow.mainnetAccessNode", DefaultMainnetAccessNode)
+	viper.SetDefault("flow.testnetAccessNode", DefaultTestnetAccessNode)
+	viper.SetDefault("flow.crescendoAccessNode", DefaultCrescendoAccessNode)
+	viper.SetDefault("flow.emulatorAccessNode", DefaultEmulatorAccessNode)
+	viper.SetDefault("flow.useCrescendo", false)
+	viper.SetDefault("flow.useEmulator", false)
 
 	var config FlowEventsConnectorConfig
 	// If available, use env vars for config
@@ -126,56 +135,4 @@ func getFlattenedStructFieldsHelper(t reflect.Type, prefixes []string) []string 
 	}
 
 	return flattenedFields
-}
-
-func GetControllerConfig() (*types.ControllerConfig, error) {
-	// Configure logger
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	zerolog.DurationFieldUnit = time.Millisecond
-	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-	zerolog.SetGlobalLevel(zerolog.Level(zerolog.DebugLevel))
-	if zerolog.GlobalLevel() == zerolog.DebugLevel {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	}
-
-	gURL, ok := os.LookupEnv("gateway_url")
-
-	if !ok {
-		return nil, fmt.Errorf("gateway_url environment variable not set")
-	}
-
-	asynchronousInvocation := true
-	if val, exists := os.LookupEnv("asynchronous_invocation"); exists {
-		asynchronousInvocation = (val == "1" || val == "true")
-	}
-
-	contentType := "text/plain"
-	if v, exists := os.LookupEnv("content_type"); exists && len(v) > 0 {
-		contentType = v
-	}
-
-	var printResponseBody bool
-	if val, exists := os.LookupEnv("print_response_body"); exists {
-		printResponseBody = (val == "1" || val == "true")
-	}
-
-	rebuildInterval := time.Second * 10
-
-	if val, exists := os.LookupEnv("rebuild_interval"); exists {
-		d, err := time.ParseDuration(val)
-		if err != nil {
-			return nil, err
-		}
-		rebuildInterval = d
-	}
-
-	return &types.ControllerConfig{
-		RebuildInterval:         rebuildInterval,
-		GatewayURL:              gURL,
-		AsyncFunctionInvocation: asynchronousInvocation,
-		ContentType:             contentType,
-		PrintResponse:           true,
-		PrintResponseBody:       printResponseBody,
-		PrintRequestBody:        false,
-	}, nil
 }
